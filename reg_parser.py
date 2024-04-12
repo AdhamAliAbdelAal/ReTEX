ee035c310d49038840e9d771e0fe4206ec976341
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2024-04-12 13:47:57 +0200

    add grammar to parser

--- reg_parser.py
+++ reg_parser.py
@@ -1,5 +1,130 @@
 import json
 
+class Validator:
+
+    alphanumeric = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    dot = list('.')
+    parentheses = list('()')
+    brackets = list('[]')
+    operators = list('|&*-?+')
+    terminals = alphanumeric + dot + parentheses + brackets + operators
+
+    S = '_S'
+    A = '_A'
+    B = '_B'
+    E = '_E'
+    R = '_R'
+    G = '_G'
+    T = '_T'
+    C = '_C'
+
+    rules = {
+        S: [[S, '|', A], [A]],
+        A: [[A, B], [B]],
+        B: [[E, '*'], [E, '+'], [E, '?'], [E]],
+        E: [['(', S, ')'], ['[', R, ']'], [T]],
+        R: [[R, G], [G]],
+        G: [[C, '-', C], [C]],
+        T: [[C], ['.']],
+        C: [[c] for c in alphanumeric]
+    }
+
+    rule2Index = {
+        S: 0,
+        A: 1,
+        B: 2,
+        E: 3,
+        R: 4,
+        G: 5,
+        T: 6,
+        C: 7
+    }
+    index2Rule = {v: k for k, v in rule2Index.items()}
+
+    @staticmethod
+    def is_terminal(c: str) -> bool:
+        return c in Validator.terminals
+
+    @staticmethod
+    def all_terminals(rule: list[str]) -> bool:
+        return all([Validator.is_terminal(c) for c in rule])
+
+    def contains_terminals(rule: list[str]) -> bool:
+        return any([Validator.is_terminal(c) for c in rule])
+
+    @staticmethod
+    def get_indices_inbound(c: str, l: int, r: int, symbolIndices: dict[str, list[int]]) -> list[int]:
+        if c not in symbolIndices:
+            return []
+        return [i for i in symbolIndices[c] if l <= i <= r]
+
+    @staticmethod
+    def solve(left: int, right:int, idx: int, reg: str, symbolIndices: dict[str, list[int]], mem) -> bool:
+        if mem[left][right][idx] != -1:
+            return mem[left][right][idx]
+        if left > right:
+            mem[left][right][idx] = False
+            return mem[left][right][idx]
+        rule = Validator.index2Rule[idx]
+        for r in Validator.rules[rule]:
+            # case1 r contains terminals only
+            # base case
+            if Validator.all_terminals(r):
+                if reg[left:right+1] == ''.join(r):
+                    mem[left][right][idx]= True
+                    return mem[left][right][idx]
+            # case2 r contains terminals
+            elif Validator.contains_terminals(r):
+                # brackets
+                if Validator.is_terminal(r[0]) and Validator.is_terminal(r[-1]):
+                    terminal_indices_left = Validator.get_indices_inbound(r[0], left, right, symbolIndices)
+                    terminal_indices_right = Validator.get_indices_inbound(r[-1], left, right, symbolIndices)
+                    if len(terminal_indices_left) > 0 and len(terminal_indices_right)>0 and left == terminal_indices_left[0] and right == terminal_indices_right[-1] and Validator.solve(left+1, right-1, Validator.rule2Index[r[1]],reg,symbolIndices,mem):
+                        mem[left][right][idx]= True
+                        return mem[left][right][idx]
+                else:
+                    # r[1] is a terminal
+                    terminal_indices = Validator.get_indices_inbound(r[1], left, right, symbolIndices)
+                    for i in terminal_indices:
+                        if Validator.solve(left, i-1, Validator.rule2Index[r[0]],reg,symbolIndices,mem):
+                            if len(r) == 2:
+                                if i == right:
+                                    mem[left][right][idx]=True
+                                    return mem[left][right][idx]
+                            elif Validator.solve(i+1, right, Validator.rule2Index[r[2]],reg,symbolIndices,mem):
+                                mem[left][right][idx]=True
+                                return mem[left][right][idx]
+            # case3 r contains non-terminals only
+            else:
+                # match all string
+                if len(r) == 1:
+                    if Validator.solve(left, right, Validator.rule2Index[r[0]],reg,symbolIndices,mem):
+                        mem[left][right][idx] =True
+                        return mem[left][right][idx]
+                else:
+                # match till right-1
+                    for i in range(left, right):
+                        if Validator.solve(left, i, Validator.rule2Index[r[0]],reg,symbolIndices,mem) and Validator.solve(i+1, right, Validator.rule2Index[r[1]],reg,symbolIndices,mem):
+                            mem[left][right][idx]=True
+                            return mem[left][right][idx]
+        mem[left][right][idx]= False
+        return False
+    @staticmethod
+    def fill_symbol_indices(reg: str) -> dict[str, list[int]]:
+        symbolIndices = {}
+        for i,c in enumerate(reg):
+            if c not in symbolIndices:
+                symbolIndices[c] = [i]
+            else:
+                symbolIndices[c].append(i)
+        return symbolIndices
+    
+    @staticmethod
+    def validate(reg: str) -> bool:
+        symbolIndices = Validator.fill_symbol_indices(reg)
+        mem = [[[-1 for _ in range(len(Validator.rules))] for _ in range(len(reg))] for _ in range(len(reg))]
+        return Validator.solve(0, len(reg)-1, Validator.rule2Index[Validator.S], reg, symbolIndices, mem)
+
 
 class Operator:
     @staticmethod
@@ -165,6 +290,8 @@ class FA:
 
 class RegParser:
     def __init__(self, text):
+        if not Validator.validate(text):
+            raise Exception("Invalid Regular Expression")
         self.text = Preprocessor.preprocess(text)
         print(self.text)
         self.build()
@@ -266,14 +393,11 @@ class RegParser:
         for s in self.states:
             ret.update(s.to_json())
         return ret
+    
 
 
 if __name__ == '__main__':
-    # text = Preprocessor.preprocess("a((b?|c)(2*u))d*[a-zA-C]")
-    # text = Preprocessor.preprocess("[a-zA-Z]")
-    # text = Preprocessor.preprocess("a(b?|c)")
-    # parser = RegParser("[a-zA-Z]").parse()
-    parser = RegParser("a*[0b-c1-5i]?")
+    parser = RegParser("[A-Za-z]+[0-9]*")
     ans = parser.parse()
     print(ans)
     # parser = RegParser("a((b?|c)(2*u))d*[a-zA-C]").parse()
